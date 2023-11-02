9cfb48ee72bdbc8c729864f6c1e9dc8c989a8fe8
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-11-02 13:24:10 +0900

    새 날개짓 애니메이션

--- bird.py
+++ bird.py
@@ -5,16 +5,20 @@ import game_framework
 
 # Move Speed
 PIXEL_PER_METER = 10.0 / 0.3
-RUN_SPEED_KMPH = 30.0
-RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
-RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
-RUN_SPEED_PPS = RUN_SPEED_MPS * PIXEL_PER_METER
+SPEED_KMPH = 30.0
+SPEED_MPM = (SPEED_KMPH * 1000.0 / 60.0)
+SPEED_MPS = (SPEED_MPM / 60.0)
+SPEED_PPS = SPEED_MPS * PIXEL_PER_METER
 
 # Action Speed
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
 FRAMES_PER_TIME = FRAMES_PER_ACTION * ACTION_PER_TIME
+
+# 2m
+SIZE_W = PIXEL_PER_METER * 2
+SIZE_H = PIXEL_PER_METER * 2
 
 
 class FlyState:
@@ -28,13 +32,20 @@ class FlyState:
 
     @staticmethod
     def do(bird):
-        bird.frame = (bird.frame + FRAMES_PER_TIME * game_framework.frame_time) % 8
-        bird.x += bird.dir * RUN_SPEED_PPS * game_framework.frame_time
+        bird.frame = (bird.frame + FRAMES_PER_TIME * game_framework.frame_time) % 14
+        bird.x += bird.dir * SPEED_PPS * game_framework.frame_time
 
 
     @staticmethod
     def draw(bird):
-        bird.image.clip_draw(int(bird.frame) * 100, bird.action * 100, 100, 100, bird.x, bird.y)
+        # bird.image.clip_draw(0, 0, 183, 168, bird.x, bird.y, SIZE_W, SIZE_H)
+        flip = 'h' if bird.dir == -1 else ''
+        if bird.frame < 4:
+            bird.image.clip_composite_draw(int(bird.frame) * 183, 0, 183, 168, 0, flip, bird.x, bird.y, SIZE_W, SIZE_H)
+        elif bird.frame < 9:
+            bird.image.clip_composite_draw((int(bird.frame)-4) * 183, 170, 183, 168, 0, flip, bird.x, bird.y, SIZE_W, SIZE_H)
+        else:
+            bird.image.clip_composite_draw((int(bird.frame)-9) * 183, 340, 183, 168, 0, flip, bird.x, bird.y, SIZE_W, SIZE_H)
 
 
 class StateMachine:
@@ -63,8 +74,7 @@ class Bird:
             Bird.image = load_image('bird_animation.png')
         self.x, self.y, self.velocity = x, y, velocity
         self.frame = 0
-        self.action = 3
-        self.dir = 0
+        self.dir = 1
         self.state_machine = StateMachine(self)
         self.state_machine.start()
 
